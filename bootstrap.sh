a429bf255cf4c5bb6cd7724c80b73afa966de18b
Bernard <pirlea.bernard@gmail.com>
2019-03-09 19:32:35 +0200

    new format read

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-awk '{ print $0}' $1
+while IFS= read -r line ; do
+	echo "$line"
+	var1=$(echo $line | cut -f1 -d:)
+	var2=$(echo $line | cut -f2 -d:)
+	echo $var1
+	echo $var2
+done <"$1"
+
