8c2eb2b50fa6c63b008ea645ba76e43ea164876f
Bernard <pirlea.bernard@gmail.com>
2019-03-09 20:07:34 +0200

    working update software

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while IFS= read -r line ; do
-	echo "$line"
 	var1=$(echo $line | cut -f1 -d:)
 	var2=$(echo $line | cut -f2 -d:)
-	echo $var1
-	echo $var2
+	if [[ $var1 == *"update"*  ]] && [[ $var2 == *"yes"* ]]; then
+		yum -y update
+	fi
 done <"$1"
 
